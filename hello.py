f5c4fc2af13300c1029fa61e2b8dfea3383adf44
drew <xj.drew@gmail.com>
2013-04-08 10:20:56 +0800

    return json data

--- hello.py
+++ hello.py
@@ -3,13 +3,15 @@
 from flask import Flask
 from flask import render_template
 from flask import request, Response
+from flask import jsonify
+from flask import json
 
 from functools import wraps
 
 app = Flask(__name__)
 
 def check_auth(username, password):
-    return username == 'xjdrew' and password == 'test1234'
+    return username == 'xjdrew' and password == 'test123'
 
 def authenticate():
     '''Sends a 401 response that enables basic auth'''
@@ -21,6 +23,7 @@ def requires_auth(f):
     def decorated(*args, **kwargs):
         auth = request.authorization
         if not auth or not check_auth(auth.username, auth.password):
+            # app.logger.debug("need authenticate")
             return authenticate()
         return f(*args, **kwargs)
     return decorated
@@ -39,6 +42,22 @@ def about():
 def hello_world(name = None):
     login_name = request.authorization.username
     return render_template('hello.html', name = name, login_name = login_name)
+
+@app.route('/json/')
+def json_data():
+    a = {}
+    a["name"] = "xjdrew"
+    a["area"] = "gz.china"
+    a["month"] = 12
+    a["members"] = ["tracy", "esther"]
+    b = {}
+    b[0] = 10
+    b[1] = 20
+    b['a'] = a
+    c = [1,2,3,4]
+    # return app.response_class(json.dumps(b, indent = 2), mimetype='application/json')
+    # return jsonify(b)
+    return Response(json.dumps(b, indent = 2), mimetype='application/json') 
 
 if __name__ == '__main__':
     app.debug = True
